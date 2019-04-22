b259e95af722018d94206e2d8e627c0ceece94bf
hulajesus <hulajesus@gmail.com>
2019-04-22 20:44:54 +0800

    upload dist

---
+++ build/precache-manifest.4a914a13e11f29cf4231a866fecec84c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3fc3966fc5e8169497644938895f93",
+    "url": "/cosmos/index.html"
+  },
+  {
+    "revision": "271c18a774e8cb5021ed",
+    "url": "/cosmos/static/css/main.02c1f35f.chunk.css"
+  },
+  {
+    "revision": "8d181be1acf35f68dcfe",
+    "url": "/cosmos/static/js/2.85e354e8.chunk.js"
+  },
+  {
+    "revision": "271c18a774e8cb5021ed",
+    "url": "/cosmos/static/js/main.298b0426.chunk.js"
+  },
+  {
+    "revision": "9f0e0699e6d67b9d0eb9",
+    "url": "/cosmos/static/js/runtime~main.0778009f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee143b6444548c9a5d25cb7ec5e0d71e.js"
+  "/cosmos/precache-manifest.4a914a13e11f29cf4231a866fecec84c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cosmos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

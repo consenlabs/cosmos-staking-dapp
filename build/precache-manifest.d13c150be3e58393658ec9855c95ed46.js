89df8e1641509f026d4045d6c2611dcec0655ae7
qw.s <sapjax@gmail.com>
2019-04-22 21:14:56 +0800

    build

---
+++ build/precache-manifest.d13c150be3e58393658ec9855c95ed46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b609933b262b6e290333f726f3a40341",
+    "url": "/cosmos/index.html"
+  },
+  {
+    "revision": "68fe3db00c8289ff52a6",
+    "url": "/cosmos/static/css/main.07cd4ced.chunk.css"
+  },
+  {
+    "revision": "531b7e4b0cad5bdc0a46",
+    "url": "/cosmos/static/js/2.cbc4f0f3.chunk.js"
+  },
+  {
+    "revision": "68fe3db00c8289ff52a6",
+    "url": "/cosmos/static/js/main.0e5ddef1.chunk.js"
+  },
+  {
+    "revision": "9f0e0699e6d67b9d0eb9",
+    "url": "/cosmos/static/js/runtime~main.0778009f.js"
+  }
+]);

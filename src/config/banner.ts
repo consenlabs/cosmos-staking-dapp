67046b57e0b56467777deb6a45f60426281c3319
Ashu <orangehat.rb@gmail.com>
2019-05-07 18:35:29 +0800

    Add activity config

--- src/config/banner.ts
+++ src/config/banner.ts
@@ -7,6 +7,21 @@ export default [
       zh_small: require('../assets/banners/hash-zh-small.png'),
       en_big: require('../assets/banners/hash-en-big.png'),
       en_small: require('../assets/banners/hash-en-small.png'),
+    },
+    activity: {
+      name: {
+        zh: '首月最高返 1.5 倍佣金',
+        en: '',
+      },
+      time: {
+        start: '',
+        end: '',
+      },
+      url: '',
+    },
+    desc: {
+      zh: 'HashQuark 是专注于PoS、DPoS 以及其他共识机制公链的矿池，拥有专业的技术背景和丰富的经验。HashQuark 由香港金融科技公司 HashKey Group 出资成立。',
+      en: '',
     }
   },
   {
@@ -17,6 +32,21 @@ export default [
       zh_small: require('../assets/banners/sparkpool-zh-small.png'),
       en_big: require('../assets/banners/sparkpool-en-big.png'),
       en_small: require('../assets/banners/sparkpool-en-small.png'),
+    },
+    activity: {
+      name: {
+        zh: '免佣金，高收益',
+        en: '',
+      },
+      time: {
+        start: '',
+        end: '',
+      },
+      url: '',
+    },
+    desc: {
+      zh: '星火矿池是全球第二大以太坊矿池，也是全球最大的 GPU 通用算力平台。三年来，星火矿池先后推出了 ETH 矿池、Beam 矿池、Grin 矿池、Cosmos 权益池以及 SparkOS 等产品，是全球范围内的顶尖矿池团队。',
+      en: '',
     }
   },
 ]

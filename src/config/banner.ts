e11a574a4988a9038f832793ff6ce173c0095d84
Ashu <orangehat.rb@gmail.com>
2019-05-08 17:23:03 +0800

    Update activity config

--- src/config/banner.ts
+++ src/config/banner.ts
@@ -11,17 +11,17 @@ export default [
     activity: {
       name: {
         zh: '首月最高返 1.5 倍佣金',
-        en: '',
+        en: 'Stake ATOM, 0% Commission + 5% Bonus ',
       },
       time: {
-        start: '',
-        end: '',
+        start: '1556035200000',
+        end: '1558627200000',
       },
-      url: '',
+      url: 'https://support.token.im/hc/articles/360022583374',
     },
     desc: {
       zh: 'HashQuark 是专注于PoS、DPoS 以及其他共识机制公链的矿池，拥有专业的技术背景和丰富的经验。HashQuark 由香港金融科技公司 HashKey Group 出资成立。',
-      en: '',
+      en: 'HashQuark is a mining pool that focuses on PoS, DPoS and other public chains consensus mechanisms. Besides Cosmos, HashQuark is also supporting nodes and masternode of many public chains including IRISnet, Cybex, CyberMiles, V SYSTEMS, VeChain, etc.',
     }
   },
   {
@@ -36,17 +36,17 @@ export default [
     activity: {
       name: {
         zh: '免佣金，高收益',
-        en: '',
+        en: 'Stake ATOM, Pay 0% Commission',
       },
       time: {
-        start: '',
-        end: '',
+        start: '1556035200000',
+        end: '1558627200000',
       },
-      url: '',
+      url: 'https://support.token.im/hc/articles/360022583274',
     },
     desc: {
       zh: '星火矿池是全球第二大以太坊矿池，也是全球最大的 GPU 通用算力平台。三年来，星火矿池先后推出了 ETH 矿池、Beam 矿池、Grin 矿池、Cosmos 权益池以及 SparkOS 等产品，是全球范围内的顶尖矿池团队。',
-      en: '',
+      en: 'SparkPool is the second largest Ethereum mining pool in the world. For the starting Proof-Of-Stake blockchain era, SparkPool is using its accumulated experience to provide users with the best service for Cosmos PoS.',
     }
   },
 ]

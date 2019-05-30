22c161449f67e42a6bc4bc1d989dea2a88053249
chezhe <chezhe17@gmail.com>
2019-05-30 14:33:27 +0800

    Style: update i18n and remove unused style file

--- src/locale/en_US.js
+++ src/locale/en_US.js
@@ -58,6 +58,11 @@ const en_US = {
   withdraw_reward_desc: 'Withdraw reward $s and pay transactions fee $s',
   reinvest_reward_desc: 'Withdraw & delegate rewards to the origin validator $s. You may pay transaction fee $s',
   no_rewards: 'No Rewards',
+  understood: 'Understood',
+  update_app_title: 'Update imToken to latest version',
+  update_app_desc: 'Current imToken doesn\'t work with this dapp. Please visit https://token.im via system browser and download the latest version',
+  no_wallet_title: 'No COSMOS wallets found',
+  no_wallet_desc: 'Please create COSMOS wallet in wallets manage with V2.4.0 or above version',
 }
 
 export default en_US

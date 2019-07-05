dbeac85110607bb062a9b4b8ed429651d1349043
xwartz <stddup@gmail.com>
2019-07-05 16:39:14 +0800

    chore: update i18n

--- src/locale/en_US.js
+++ src/locale/en_US.js
@@ -77,6 +77,7 @@ const en_US = {
   redelegate_state: 'The re-delegation will take effect immediately, i.e. your re-delegated ATOM will earn rewards. But, it will be locked for 21 days, meaning you won\'t be able to re-delegation again for 21 days.',
   more_details: 'More details here.',
   campaign_over: 'The event has ended',
+  search_validator: 'Search validator',
 }
 
 export default en_US
